dc9dce02269842348df7cf079cc9c16560283ceb
dlsso <supertonesfan@hotmail.com>
2014-08-12 18:32:51 -0600

    Change to console log

--- src/play.js
+++ src/play.js
@@ -92,7 +92,7 @@ Game.Play.prototype = {
 			updateCounter = 0
 		}
 		else if(player.key === 'player2') {
-			if(player.inAir === false) { player.y = player.y + 2}	
+			if(player.inAir === false) { console.log(player.y)}	
 			player.loadTexture('player3', 1)
 			updateCounter = 0
 		}
